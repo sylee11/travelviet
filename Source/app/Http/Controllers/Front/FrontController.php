6ecf6fdb8e9c6138b89c059b73075f3fffbc6294
ledt <ledt@runsystem.net>
2019-07-09 10:38:29 +0700

    show home

--- Source/app/Http/Controllers/Front/FrontController.php
+++ Source/app/Http/Controllers/Front/FrontController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers\Front;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Post;
+use App\Rating;
+use DB;
 class FrontController extends Controller
 {
-    public function index(){
-        return view('pages.home');
-    }
+	public function index(){
+		$new_post= Post::orderBy('id', 'desc')->take(3)->get();
+		 $rating=Rating::orderBy('rating','desc')->take(3)->get();
+		 $top_user=Post::select('user_id', \DB::raw('count(id) as amount'))->groupBy('user_id')->orderBy('amount','desc')->take(4)->get();
+		 foreach ($new_post as $value) {
+		 	 $rat =Rating::where('post_id', '=', $value->id)->get();
+		 }
+
+		 return view('pages.index',['a'=>$rat,'rating'=>$rating,'top_user'=>$top_user]);
+	}
 }
---
+++ Source/resources/views/pages/index.blade.php
@@ -0,0 +1,99 @@
+@extends('pages.home')
+<style type="text/css" >
+	.list_images {
+		opacity: 0.7;
+	}
+
+	.list_images:hover {
+		opacity: 1;
+	}
+	p{
+		color: #090707;
+	}
+	.checked {
+		color: orange;
+	}
+</style>
+@section('content-section')
+<div class="container">
+	<div style="text-align: left;margin-top:50px;"><h2>Top Reviewer</h2></div>
+
+	<div class="row">
+		@if($rating)
+		@foreach ($rating as $record)
+		<div class="col-sm-4" style="margin:50px 0;">
+			<div class="card" style="width: 20rem;height:350px;">
+				<a href="#" title="" style="text-decoration: none;">
+					<img class="card-img-top list_images" src="{{ asset('picture/front/image1.jpg') }}" alt="smaple image">
+
+					<div class="card-body" >
+
+						<h5 class="card-title text-primary">{{$record->post->title}}</h5>
+						<div class="rating">
+							@for($i=0;$i< $record->rating;$i++)
+							<span class="fa fa-star checked" ></span>
+							@endfor
+							@for($i=$record->rating;$i< 5;$i++)
+							<span class="fa fa-star" ></span>
+							@endfor
+						</div>
+
+						<p class="card-text">
+						</p>
+
+					</div>
+				</a>
+
+			</div>
+		</div>
+		@endforeach
+		@endif
+	</div>
+</div>
+
+<div class="container">
+	<div style="text-align: left;margin:50px 0;"><h2>New posts</h2></div>
+	<div class="row">
+		@if($a)
+		@foreach ($a as $record)
+		<div class="col-sm-4" style="margin:50px 0;">
+			<div class="card" style="width: 20rem;height:350px;">
+				<a href="#" title="" style="text-decoration: none;">
+					<img class="card-img-top list_images" src="{{ asset('picture/front/image2.jpg') }}" alt="smaple image">
+
+					<div class="card-body">
+
+						<h5 class="card-title text-primary">{{$record->post->title}}</h5>
+						
+						<p class="card-text">
+						</p>
+
+					</div>
+				</a>
+
+			</div>
+		</div>
+		@endforeach
+		@endif
+	</div>
+</div>
+
+<div class="container">
+	<div style="text-align: left;margin:50px 0;"><h2>Top Mod</h2></div>
+	<div class="row" style="margin:0 300px;">
+		@if($top_user)
+		@foreach($top_user as $record)
+		<div class="col-sm-3">
+			<a href="#" title="" style="text-decoration: none;">
+				@if (!empty($record->user->avatar))
+				<img src="{{ $record->user->avatar }}" alt="Avatar" class="avatar" style="width: 80px;height:80px;border-radius: 50%;">
+				@else
+				<img src="{{ asset('picture/images.png') }}" alt="Avatar" class="avatar" style="width: 80px;height:80px;border-radius: 50%;">
+				@endif
+			</a>
+		</div>
+		@endforeach
+		@endif
+	</div>
+</div>
+@endsection
