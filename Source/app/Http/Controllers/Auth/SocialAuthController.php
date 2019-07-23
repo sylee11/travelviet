ff56219ac3d6f0a02ed0077cbb771a9e6c8784e2
ledt <ledt@runsystem.net>
2019-07-23 17:23:49 +0700

    fix login google

--- Source/app/Http/Controllers/Auth/SocialAuthController.php
+++ Source/app/Http/Controllers/Auth/SocialAuthController.php
@@ -4,88 +4,120 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use  Auth, Redirect, Session, URL;
+use Google_Client;
+use Google_Service_Plus;
 use App\User;
 use App\Social;
-use Image;
+
 class SocialAuthController extends Controller
 {
+    public function redirectToProvider()
+    {
+    	$client = new Google_Client();
+    	$client->setRedirectUri(env('GOOGLE_REDIRECT'));
+    	$client->addScope([
+    		'https://www.googleapis.com/auth/plus.login',
+    		'https://www.googleapis.com/auth/userinfo.email'
+    	]);
 
-	public function redirectToProvider()
-	{
-		$authorizeURL = 'https://accounts.google.com/o/oauth2/v2/auth';
-		$state= bin2hex(random_bytes(16));
-		session_start();
-		$params = array(
-			'response_type' => 'code',
-			'client_id' => env('GOOGLE_ID'),
-			'redirect_uri' => env('GOOGLE_REDIRECT'),
-			'scope' => 'openid email profile',
-			'state' => $state
-		);
+    //Set param google API
+    	$client->setClientId(env('GOOGLE_ID'));
+    	$client->setClientSecret(env('GOOGLE_SECRET'));
+    	$client->setAccessType('offline');
+    	//Đây là URL bạn cần mở nếu chưa đăng nhập
+    	$auth_url = $client->createAuthUrl();
 
-  		// Redirect the user to Google's authorization page
-		header('Location: '.$authorizeURL.'?'.http_build_query($params));
-		die();
-	}
+    	if (isset($_SESSION['access_token']) && $_SESSION['access_token'])
+    	{
+        /*
+         * Đã đăng nhập trước rồi do tồn tại access_token trong Session
+         * Nên bạn không cần xác thực từ Google nữa mà chỉ việc lấy thông tin
+         */
+        $client->setAccessToken($_SESSION['access_token']);
+        $plus = new Google_Service_Plus($client);
+        if ($client->isAccessTokenExpired()) {
+            //Truy cập bị hết hạn, cần xác thực lại
+            //Chuyển hướng sang Google để lấy xác thực
+        	$auth_url = $client->createAuthUrl();
+        	header("Location: $auth_url");
+        	die();
+        }
+		 //Lấy các thông tin của User
+        $me = $plus->people->get('me');
+        $email = @$me['emails'][0]['value'];
+        $existingUser = User::where('email', $email)->first();
+        auth()->login($existingUser, true);
+        //Sau khi lấy được thông tin xử lý xong nhớ chuyển hướng sang trang khác.
+        return redirect('/');
+    }
+    else
+    {
+        /**
+         * Nếu tồn tại $_GET['code'] trên URL có nghĩa là Google vừa gửi Code truy cập tới cho bạn, bạn cần lấy thông
+         * tin này để truy cập.
+         */
+        if (isset($_GET['code'])) {
+        	$client->fetchAccessTokenWithAuthCode($_GET['code']);
+            //Lấy mã Token và lưu lại tại SESSION
+        	$_SESSION['access_token'] = $client->getAccessToken();
+        	//getinfo($client);
 
-	public function handleProviderCallback()
 
-	{
-		if(isset($_GET['code'])) {
-			$tokenURL = 'https://www.googleapis.com/oauth2/v4/token';
-			$ch = curl_init($tokenURL);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-				'grant_type' => 'authorization_code',
-				'client_id' => env('GOOGLE_ID'),
-				'client_secret' => env('GOOGLE_SECRET'),
-				'redirect_uri' => env('GOOGLE_REDIRECT'),
-				'code' => $_GET['code'],
-			]));
-		$response = curl_exec($ch);//dang json
-		$data = json_decode($response, true);//chuyen tu json sang array
-		$token = explode('.', $data['id_token']);
-		$userinfo = json_decode(base64_decode($token[1]), true);
-		//dd($userinfo);
-		$social_id=$userinfo['sub'];
-		$_SESSION['access_token'] = $data['access_token'];
-	}
-	//gui access_token de lay thong tin ung dung
-	if(!empty($social_id)) {
-		//cap quyen truy cap info user khi scope o tren khong co profile
-		$ch = curl_init('https://openidconnect.googleapis.com/v1/userinfo');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, [
-			'Authorization: Bearer '.$_SESSION['access_token']
-		]);
-		$user= json_decode(curl_exec($ch),true);
-		//dd($user);
-		// kiem tra mail da ton tai chua
-		$existingUser = User::where('email', $userinfo['email'])->first();
-		if($existingUser){
+        	$client->setAccessToken($_SESSION['access_token']);
+        	$plus = new Google_Service_Plus($client);
+        	if ($client->isAccessTokenExpired()) {
+            //Truy cập bị hết hạn, cần xác thực lại
+            //Chuyển hướng sang Google để lấy xác thực
+        		$auth_url = $client->createAuthUrl();
+        		header("Location: $auth_url");
+        		die();
+        	}
+
+
+        //Lấy các thông tin của User
+        	$me = $plus->people->get('me');
+        $id    = @$me['id'];                    //ID
+        $email = @$me['emails'][0]['value'];    //Địa chỉ email
+        $name  = @$me['displayName'];           //Tên
+        $image = @$me['image']['url'];          //Url của ảnh avatar
+
+        $existingUser = User::where('email', $email)->first();
+        if($existingUser){
 			// kiem tra social_id da ton tai chua
-			auth()->login($existingUser, true);
-		} else {
+        	auth()->login($existingUser, true);
+        } else {
             // tao 1 user moi vao db
 
-			$newUser = new User;
-			$newUser->email = $user['email'];
-			$newUser->avatar = $user['picture'];
-		 	$newUser->save();
+        	$newUser = new User;
+        	$newUser->name= $name;
+        	$newUser->email = $email;
+        	$newUser->avatar = $image;
+        	$newUser->save();
 
 			$userId = User::all()->last()->id;//truy van toi record cuoi
 			//dd($userId );
 			$newSocial = new Social;
-			$newSocial->id= $social_id;
+			$newSocial->id= $id ;
 			$newSocial->user_id= $userId;
 			$newSocial->save();
 
 
 			auth()->login($newUser, true);
 		}
+
+        	//dd($_SESSION['access_token']);
+            //Sau khi lấy được thông tin xử lý xong nhớ chuyển hướng sang trang khác.
 		return redirect('/');
 	}
+	else
+	{
+            //Chuyển hướng sang Google để lấy xác thực
+		$auth_url = $client->createAuthUrl();
+		header("Location: $auth_url");
+		die();
+	}
+}
+
 }
 
 }
