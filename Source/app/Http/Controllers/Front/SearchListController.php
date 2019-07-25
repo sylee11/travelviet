c714a15ae755460eba820ad7436e2fec0590029f
namnk <namnk@runsystem.net>
2019-07-25 08:41:02 +0700

    b

--- Source/app/Http/Controllers/Front/SearchListController.php
+++ Source/app/Http/Controllers/Front/SearchListController.php
@@ -114,6 +114,28 @@ class SearchListController extends Controller
                  
 				return view('pages.list_place',['post' => $post]);
 			}
+			elseif($request->cities_id !='' && $request->districts_id =='' && $request->category_id !='')
+			{
+				$post = DB::table('posts')
+				->join('places','posts.place_id','=','places.id')
+				->join('districts','places.districts_id','=','districts.id')
+				->join('cities','districts.cities_id','=','cities.id')
+				->join('categories','places.category_id','=','categories.id')
+				->leftjoin('ratings', 'posts.id', '=', 'ratings.post_id')
+				->join('photos', 'posts.id', '=', 'photos.post_id')
+				->select('posts.id', 'posts.title','posts.describer','places.address','photos.photo_path',\DB::raw('avg(ratings.rating) as avg_rating'))->groupBy('posts.id')->groupBy('posts.title')->groupBy('photos.photo_path')->groupBy('posts.describer')->groupBy('places.address')
+				->where('photos.flag', '=', '1')
+				->where([
+
+					['cities_id','=', $request->cities_id],
+					['category_id','=', $request->category_id],
+				])
+
+				// ->get();
+				->Paginate(10);
+                 
+				return view('pages.list_place',['post' => $post]);
+			}
 
 			
 		}
