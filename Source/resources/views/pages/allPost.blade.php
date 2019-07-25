ceff4ae3b4f26b2eb6b523e122f2359b6fdb96ba
ledt <ledt@runsystem.net>
2019-07-25 17:35:23 +0700

    fix loi tim kiem

--- Source/resources/views/pages/allPost.blade.php
+++ Source/resources/views/pages/allPost.blade.php
@@ -11,56 +11,57 @@
 		value="" placeholder="Nhập từ khóa" style="width: 300px;">
 		<button type="submit" class="btn-success btn" style="margin-left: 20px;"> Search</button>
 	</form>
-		@if($search != NUll)
-		<div class="font-weight-bold">
-			Đã tìm thấy <span style="color: green;">{{$all_posts->count()}}</span> kết quả  cho từ khóa <span style="color: green"> "{{$search}}"</span>
-		</div>
-		@endif
-		<div class="row" style="justify-content: center;">
-
-
-
-			@if($all_posts->count() !== 0)
-			@foreach ($all_posts as $record)
-			<div class="col-sm-4" style="margin:50px 0;">
-				<div class="card-img" id="card-img" >
-					<a href="{{route('detail',$record->id)}}" title="" style="text-decoration: none;"id="pic">
-						<div style="height: 200px;">
-							<img class="card-img-top list_images" src="{{ $record->photo_path }}" alt="{{$record->title}} " >
-						</div>
-
-						<div class="card-body">
-
-							<h5 class="card-title text-primary">
-
-								<span style="display:block;text-overflow: ellipsis;overflow: hidden; white-space: nowrap;font-size: 16px;color: black;">
-									{{$record->title}}
-								</span>
-							</h5>
-							<div class="rating">
-								@for($i=0;$i< ceil($record->avg_rating);$i++)
-								<span class="fa fa-star checked" ></span>
-								@endfor
-								@for($i=ceil($record->avg_rating);$i< 5;$i++)
-								<span class="fa fa-star unchecked" ></span>
-								@endfor
-							</div>
-
-							<p class="card-text">
-							</p>
-
-						</div>
-					</a>
-
-				</div>
-			</div>
-			@endforeach
-		</div>
-		@else
-		<div class="col-sm">
-			<p>Không có dữ liệu</p>
-		</div>
-		@endif
+	@if(isset($search))
+	<div class="font-weight-bold">
+		Đã tìm thấy <span style="color: green;">{{$all_posts->count()}}</span> kết quả  cho từ khóa <span style="color: green"> "{{$search}}"</span>
 	</div>
-	<div style="display: inline-block;">{{$all_posts->links()}}</div>
-	@endsection
+	@endif
+
+	<div class="row" style="justify-content: center;">
+
+
+
+		@if($all_posts->count() !== 0)
+		@foreach ($all_posts as $record)
+		<div class="col-sm-4" style="margin:50px 0;">
+			<div class="card-img" id="card-img" >
+				<a href="{{route('detail',$record->id)}}" title="" style="text-decoration: none;"id="pic">
+					<div style="height: 200px;">
+						<img class="card-img-top list_images" src="{{ $record->photo_path }}" alt="{{$record->title}} " >
+					</div>
+
+					<div class="card-body">
+
+						<h5 class="card-title text-primary">
+
+							<span style="display:block;text-overflow: ellipsis;overflow: hidden; white-space: nowrap;font-size: 16px;color: black;">
+								{{$record->title}}
+							</span>
+						</h5>
+						<div class="rating">
+							@for($i=0;$i< ceil($record->avg_rating);$i++)
+							<span class="fa fa-star checked" ></span>
+							@endfor
+							@for($i=ceil($record->avg_rating);$i< 5;$i++)
+							<span class="fa fa-star unchecked" ></span>
+							@endfor
+						</div>
+
+						<p class="card-text">
+						</p>
+
+					</div>
+				</a>
+
+			</div>
+		</div>
+		@endforeach
+	</div>
+	@else
+	<div class="col-sm">
+		<p>Không có dữ liệu</p>
+	</div>
+	@endif
+</div>
+<div style="display: inline-block;">{{$all_posts->links()}}</div>
+@endsection
