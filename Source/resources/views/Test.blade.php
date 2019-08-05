5deb46c35374923ced584cad40a94acd1801c0b6
sylv@runsystem.net <anhsypro123@gmail.com>
2019-08-05 16:57:26 +0700

    Aautocomplete Post

--- Source/resources/views/Test.blade.php
+++ Source/resources/views/Test.blade.php
@@ -1,73 +1,68 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.7/css/fileinput.css" media="all" rel="stylesheet" type="text/css"/>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" media="all" rel="stylesheet" type="text/css"/>
-    <style type="text/css">
-        .main-section{
-            margin:0 auto;
-            padding: 20px;
-            margin-top: 100px;
-            background-color: #fff;
-            box-shadow: 0px 0px 20px #c1c1c1;
-        }
-        .fileinput-remove,
-        .fileinput-upload{
-            display: none;
-        }
-    </style>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Autocomplete - Default functionality</title>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
+
+  <script>
+  $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    // $( "#tags" ).autocomplete({
+    //   source: availableTags
+    // });
+  } );
+  </script>
 </head>
-<body class="bg-danger">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-sm-12 col-11 main-section">
-                <h1 class="text-center text-danger">File Input Example</h1><br>
-                
-                    {!! csrf_field() !!}
-                    <div class="form-group">
-                        <div class="file-loading">
-                            <input id="file-1" type="file" name="file" multiple class="file" data-overwrite-initial="false" data-min-file-count="2">
-                        </div>
-                    </div>
-                
-            </div>
-        </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.7/js/fileinput.js" type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.7/themes/fa/theme.js" type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" type="text/javascript"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" type="text/javascript"></script>
-
-
-    <script type="text/javascript">
-        $("#file-1").fileinput({
-            theme: 'fa',
-            uploadUrl: "/image-view",
-            uploadExtraData: function() {
-                return {
-                    _token: $("input[name='_token']").val(),
-                };
-            },
-            allowedFileExtensions: ['jpg', 'png', 'gif'],
-            overwriteInitial: false,
-            maxFileSize:2000,
-            maxFilesNum: 10,
-            slugCallback: function (filename) {
-                return filename.replace('(', '_').replace(']', '_');
-            }
-        });
-    </script>
-    <form method="GET">
-    <textarea class="form-control" rows="5" id="editor1" name="comment" required style="width: 200px;"></textarea>
-    <BUTTON>abc</BUTTON>
-    </form>
-    <?php
-            echo $_GET['comment'];
-    ?>
-        <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
-        <script type="text/javascript" src="{{asset('ckeditor/adapters/jquery.js') }}"></script>
-        <script type="text/javascript">CKEDITOR.replace('editor1');</script>
+<body>
+ 
+<div class="ui-widget">
+  <label for="tags">Tags: </label>
+  <input id="tags">
+</div>
+ <script type="text/javascript">
+  $(document).ready(function(){
+    var path = "{{ route('post.autocomplete') }}";
+    $('.tags').typeahead({
+        source:  function (query, process) {
+        return $.get(path, { query: query }, function (data) {
+                return process(data);
+            });
+        }
+    });
+    })
+</script>
+ 
 </body>
 </html>
