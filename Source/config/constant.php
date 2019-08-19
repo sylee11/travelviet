78fe1c8246f3ae8c7b6c6273a2d14b6f32f10f7c
ledt <ledt@runsystem.net>
2019-08-19 09:53:19 +0700

    fix last

--- Source/config/constant.php
+++ Source/config/constant.php
@@ -6,6 +6,15 @@
 			    'message_erros' => 'Erros',
 			    'message_fail'	=> 'Fail',
 
+			],
+			'pagination1' => 20,
+			'numberRecord1' => 4,
+			'numberRecord2' => 6,
+			'numberRecord3' => 12,
+			'rating' => [
+				'addSuccess' => 'Bạn đã thêm thành công',
+				'editSuccess' => 'Bạn đã sửa thành công',
+				'delSuccess' => 'Bạn đã xóa thành công'
 			]
 
 
