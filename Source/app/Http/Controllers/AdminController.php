7c12cb36b7775db5368fab923d7d2ef90632bc9c
minh hoang <hoangle1413@gmail.com>
2019-08-23 10:45:47 +0700

    sua loi

--- Source/app/Http/Controllers/AdminController.php
+++ Source/app/Http/Controllers/AdminController.php
@@ -9,80 +9,64 @@ use DB;
 
 class AdminController extends Controller
 {
+    private $ADMIN_VIEW = 'admin.index';
     public function index()
     {
-        return view('admin.index');
+        return view($this->ADMIN_VIEW);
     }
     public function chart()
     {
         $begin = \Request::get('begin');
         $end = \Request::get('end');
         $dbname = \Request::get('dbname');
-        if ($begin == null || $end == null) {
-            if ($end == null && $begin == null) return view('admin.index');
-            if ($begin == null) {
-                $yearend = explode('-', $end)[0];
-                $monthend = explode('-', $end)[1];
-                $data = DB::select("select COUNT(*) as count from $dbname where Year(created_at)=$yearend AND Month(created_at)=$monthend");
-                $arr[] = $monthend . '-' . $yearend;
-                $db_arr[] = $data[0]->count;
-            } else {
-                $yearbegin = explode('-', $begin)[0];
-                $monthbegin = explode('-', $begin)[1];
-                $data = DB::select("select COUNT(*) as count from $dbname where Year(created_at)=$yearbegin AND Month(created_at)=$monthbegin");
-                $arr[] = $monthbegin . '-' . $yearbegin;
-                $db_arr[] = $data[0]->count;
-            }
+        $yearbegin = explode('-', $begin)[0];
+        $monthbegin = explode('-', $begin)[1];
+        $yearend = explode('-', $end)[0];
+        $monthend = explode('-', $end)[1];
+
+        if ($yearbegin > $yearend || ($yearbegin == $yearend && $monthbegin > $monthend)) {
+            return $this->index();
+        } elseif ($yearbegin == $yearend && $monthbegin == $monthend) {
+            $data = DB::select("select COUNT(*) as count from $dbname where Year(created_at)=$yearend AND Month(created_at)=$monthend");
+            $arr[] = $monthend . '-' . $yearend;
+            $db_arr[] = $data[0]->count;
         } else {
-            $yearend = explode('-', $end)[0];
-            $monthend = explode('-', $end)[1];
-            $yearbegin = explode('-', $begin)[0];
-            $monthbegin = explode('-', $begin)[1];
-            if ($yearbegin > $yearend || ($yearbegin == $yearend && $monthbegin > $monthend)) {
-                return $this->index();
-            } elseif ($yearbegin == $yearend && $monthbegin == $monthend) {
-                $data = DB::select("select COUNT(*) as count from $dbname where Year(created_at)=$yearend AND Month(created_at)=$monthend");
-                $arr[] = $monthend . '-' . $yearend;
-                $db_arr[] = $data[0]->count;
-            } else {
 
 
-                $data = DB::select("select YEAR(created_at) as year,Month(created_at) as month,COUNT(*) as count from $dbname where created_at BETWEEN '$yearbegin-$monthbegin-01' AND '$yearend-$monthend-31' GROUP BY Year(created_at),Month(created_at)");
-               
-                $db_arr = [];
-                
-                $i = 0;
-                $arr = [];
-                
-                while ($monthbegin != $monthend || $yearbegin != $yearend) {
-                    array_push($arr, $monthbegin . '-' . $yearbegin);
-                    if ($i < count($data) && $monthbegin == $data[$i]->month && $yearbegin == $data[$i]->year) {
-                        $db_arr[] = $data[$i]->count;
-                        $i++;
-                    } else {
-                        $db_arr[] = 0;
-                    }
-                    $monthbegin++;
-                    if ($monthbegin > 12) {
-                        $monthbegin = 1;
-                        $yearbegin++;
-                    }
-                }
-                if ($i < count($data))
-                    if ($monthbegin == $data[$i]->month && $yearbegin == $data[$i]->year)
-                        $db_arr[] = $data[$i]->count;
+            $data = DB::select("select YEAR(created_at) as year,Month(created_at) as month,COUNT(*) as count from $dbname where created_at BETWEEN '$yearbegin-$monthbegin-01' AND '$yearend-$monthend-31' GROUP BY Year(created_at),Month(created_at)");
 
+            $db_arr = [];
+
+            $i = 0;
+            $arr = [];
+
+            while ($monthbegin != $monthend || $yearbegin != $yearend) {
                 array_push($arr, $monthbegin . '-' . $yearbegin);
-                
+                if ($i < count($data) && $monthbegin == $data[$i]->month && $yearbegin == $data[$i]->year) {
+                    $db_arr[] = $data[$i]->count;
+                    $i++;
+                } else {
+                    $db_arr[] = 0;
+                }
+                $monthbegin++;
+                if ($monthbegin > 12) {
+                    $monthbegin = 1;
+                    $yearbegin++;
+                }
             }
+            if ($i < count($data) && $monthbegin == $data[$i]->month && $yearbegin == $data[$i]->year) {
+                $db_arr[] = $data[$i]->count;
+            }
+            array_push($arr, $monthbegin . '-' . $yearbegin);
         }
-        $a = mt_rand(0, 255);
-        $b = mt_rand(0, 255);
-        $c = mt_rand(0, 255);
+
+        $a = random_int(0, 255);
+        $b = random_int(0, 255);
+        $c = random_int(0, 255);
         $chart = new SampleChart;
         $chart->labels($arr);
         $chart->dataset($dbname, 'bar', $db_arr)->options(['backgroundColor' => "rgb($a,$b,$c)",]);
         $chart->displayLegend(true);
-        return view('admin.index', ['chart' => $chart]);
+        return view($this->ADMIN_VIEW, ['chart' => $chart]);
     }
 }
--- Source/resources/views/admin/index.blade.php
+++ Source/resources/views/admin/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-<!-- Icon Cards-->
+
 <div class="row">
   <div class="col-xl-3 col-sm-6 mb-3">
     <div class="card text-white bg-primary o-hidden h-100">
@@ -35,19 +35,6 @@
     </div>
   </div>
 </div>
-<!-- Area Chart Example-->
-<!--        <div class="card mb-3">
-          <div class="card-header">
-            <i class="fas fa-chart-area"></i>
-            Area Chart Example</div>
-          <div class="card-body">
-            <canvas id="myAreaChart" width="100%" height="30"></canvas>
-          </div>
-          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-        </div>
-        <div class="mr-5">Posts</div>
-      </div>
--->
 
 <form action="/admin/chart" method="GET">
   <label>Table</label>
@@ -57,20 +44,11 @@
     <option value="ratings">Rating</option>
     <option value="posts">Post</option>
   </select>
-  <!--  <label>Year</label>
-  <select name="year" >
-  <option value="" selected disabled hidden>Choose here</option>
-    <option value="2019">2019</option>
-    <option value="2018">2018</option>
-    <option value=""></option>
-    <option value=""></option>
-  </select>
-
-  <input class="btn-success" type="submit">-->
+  
   <br>
 
-  <input type="month" name="begin">
-  <input type="month" name="end">
+  <input type="month" name="begin" required>
+  <input type="month" name="end" required>
   <button class="btn-success" type="submit" formaction="/admin/chart" formmethod="GET">Submit</button>
 </form>
 
